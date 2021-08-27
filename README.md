548cfa6f0d9c71eeb0beef0bf157443ad71dcc08
blackkiv <fallena238@gmail.com>
2021-08-27 19:14:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# hi-boy-bsnu-bot
+
+## how-to run
+
+1. clone git repository  
+   use: `git clone https://github.com/blackkiv/hi-boy-bsnu-bot.git`
+2. create properties.py file with field BOT_TOKEN (token should be taked from @BotFather)  
+   example: `BOT_TOKEN = '{TOKEN}'`
+3. install requirements from requirements.txt  
+   use: `pip install -r requirements.txt`
+4. run bot  
+   use: `python main.py`
---
+++ main.py
@@ -0,0 +1,27 @@
+from properties import BOT_TOKEN
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def welcome(message: types.Message):
+    await message.reply("я кароч слежу за тем, кто зашел..")
+
+
+@dp.message_handler(content_types=["new_chat_members"])
+async def chat_member(message: types.Message):
+    new_member_username = message.new_chat_members[0].username
+    chat_id = message.chat.id
+    text = f"👀✨ привет, @{new_member_username}..\nкакой курс, специальность, группа?\nизучал(а) программирование или онли школьные знания? (относится к первашам)"
+    logging.log(logging.INFO, msg=f"new chat user: {new_member_username}")
+    await bot.send_message(chat_id, text)
+
+
+if __name__ == "__main__":
+    print("start bot polling..")
+    executor.start_polling(dispatcher=dp, skip_updates=True)
