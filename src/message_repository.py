f7d29f769ad2b996b1fa6491aa87548b5c698e1d
blackkiv <fallena238@gmail.com>
2021-09-13 14:39:32 +0300

    migrate from elasticsearch to mongodb

--- src/message_repository.py
+++ src/message_repository.py
@@ -1,21 +1,21 @@
-from elasticsearch import AsyncElasticsearch
-from properties import ELASTICSEARCH_HOST
+from motor import motor_asyncio
+from properties import MONGODB_HOST,MONGODB_PORT
 
 
 class MessageRepository(object):
     def __init__(self):
-        global es
-        es = AsyncElasticsearch(hosts=[{"host": ELASTICSEARCH_HOST}])
+        global mongo
+        client = motor_asyncio.AsyncIOMotorClient(MONGODB_HOST, MONGODB_PORT)
+        print(client.server_info())
+        mongo = client["msg"]["messages"]
 
     async def save(self, chat_id: str, message: str):
-        index = f"bot{chat_id}"
-        await es.index(index, id=chat_id, body={"msg": message})
+        document = {"chat_id": chat_id, "message": message}
+        await mongo.insert_one(document)
 
-    async def get(self, chat_id: int):
-        index = f"bot{chat_id}"
-        res = await es.get(index, id=chat_id)
-        return res["_source"]["msg"]
+    async def get(self, chat_id: str):
+        result = await mongo.find_one({"chat_id": chat_id})
+        return result["message"]
 
     async def delete(self, chat_id: int):
-        index = f"bot{chat_id}"
-        await es.delete(index, id=chat_id)
+        await mongo.delete_many({"chat_id": chat_id})
--- src/properties.py
+++ src/properties.py
@@ -1,5 +1,7 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ELASTICSEARCH_HOST = os.environ.get("ELASTICSEARCH_HOST")
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
+MONGODB_PORT = os.environ.get("MONGODB_PORT")
+MONGODB_CONNECTION_STRING = f"mongodb://{MONGODB_HOST}:{MONGODB_PORT}"
 DEFAULT_MESSAGE = "👀✨ привет, {MENTION}\nкакой курс, специальность, группа?\n(ДЛЯ ФКН) -> изучал(а) программирование или онли школьные знания? (относится к первашам)"
